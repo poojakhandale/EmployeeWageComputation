924efb063a056813b31c2af3863dc48824d6ec10
poojakhandale <poojakhandale2000@gmail.com>
2023-02-09 10:10:56 +0530

    Employee Wage Computation program added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EmployeeWageComputation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WELOME TO THE EMPLOYEEWADGECOMPUTATION");
+            EmployeeCheck.CheckEmployee();
+        }
+    }
+}
+  
+       
