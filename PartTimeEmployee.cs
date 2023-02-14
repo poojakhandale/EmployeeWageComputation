f0fbf9fabfe44abfe6dee67751d638490af3b41b
poojakhandale <poojakhandale2000@gmail.com>
2023-02-14 17:41:44 +0530

    Employee is part time or full time

---
+++ PartTimeEmployee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+
+    public class PartTime
+    {
+        public static void partTime()
+        {
+            int isFullTime = 1;
+            int empRatePerHrs = 20;
+            int isPartTime = 2;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            if (empCheck == isFullTime)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == isPartTime)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * empRatePerHrs;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}   
+
+
+    
+
