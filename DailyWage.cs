c4641aeb3f7d5f02e99ef28588a53d3ab7f8b543
poojakhandale <poojakhandale2000@gmail.com>
2023-02-14 17:17:48 +0530

    Dalily wage calutatoin

---
+++ DailyWage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class DailyWage
+    {
+        public static void calculateDailywage()
+        {
+
+            int isFullTime = 1;
+            int empRatePerHrs = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == isFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * empRatePerHrs;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
+
+   
+ 
+       
+
+    
+
