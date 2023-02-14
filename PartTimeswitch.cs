863a137de78dffa8ddaf43ecf6f02b0a5950f905
poojakhandale <poojakhandale2000@gmail.com>
2023-02-14 17:51:49 +0530

    program using switch

---
+++ PartTimeswitch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class PartTimeswitch
+    {
+        
+        public const int isFullTime = 1;
+        public const int empRatePerHrs = 20;
+        public const int isPartTime = 2;
+        public static void partTime()
+        {
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            switch (empCheck)
+            {
+                case isFullTime:
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+            }
+
+            empWage = empHrs * empRatePerHrs;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+
+    }
+
